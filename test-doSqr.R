314a50f397c58418726ed4c202b9f9029d1c2782
Frank de Kievit <frank_de_kievit@hotmail.com>
2016-12-16 13:32:37 +0000

    broke unittest

--- test-doSqr.R
+++ test-doSqr.R
@@ -3,7 +3,7 @@ library("testthat")
 source("doSqr.R")
 
 test_that("test that the sqr root of 2 is 4", {
-   input <- c(2)
+   input <- c(3)
    output <- calcSqr(input)
 
    expect_that(output, equals(4))
