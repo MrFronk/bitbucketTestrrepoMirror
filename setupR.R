0854a9c0bb41cf025566f44d19179d25f6a64674
Frank de Kievit <frank_de_kievit@hotmail.com>
2016-12-16 13:50:02 +0000

    changed setup script

--- setupR.R
+++ setupR.R
@@ -1,2 +1,3 @@
-install.packages("devtools")
-install.packages("testthat")
+
+install.packages("devtools", repos="http://cran.rstudio.com/")
+install.packages("testthat", repos="http://cran.rstudio.com/")
